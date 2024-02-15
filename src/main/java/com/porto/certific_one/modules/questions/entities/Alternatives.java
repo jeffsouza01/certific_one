345d045b6ca121f9a11be7fe1b0d5b7e16042f1a
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-02-15 20:59:22 -0300

    Include question module

--- src/main/java/com/porto/certific_one/modules/questions/entities/Alternatives.java
+++ src/main/java/com/porto/certific_one/modules/questions/entities/Alternatives.java
@@ -1,2 +1,31 @@
-package com.porto.certific_one.modules.questions.entities;public class Alternatives {
+package com.porto.certific_one.modules.questions.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_alternatives")
+public class Alternatives {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String description;
+
+    private boolean isCorrect;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/porto/certific_one/modules/questions/entities/Questions.java
+++ src/main/java/com/porto/certific_one/modules/questions/entities/Questions.java
@@ -1,2 +1,34 @@
-package com.porto.certific_one.modules.questions.entities;public class Questions {
+package com.porto.certific_one.modules.questions.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_questions")
+public class Questions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(length = 50)
+    private String technology;
+
+    private String description;
+
+    @OneToMany
+    @JoinColumn(name = "question_id")
+    private List<Alternatives> alternatives;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }

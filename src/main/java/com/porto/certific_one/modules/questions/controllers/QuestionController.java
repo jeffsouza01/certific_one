859cbbbf8ece40f673634ceb78ffae44bcb4c786
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-02-20 01:07:18 -0300

    Update Question Controller with DTO

--- src/main/java/com/porto/certific_one/modules/questions/controllers/QuestionController.java
+++ src/main/java/com/porto/certific_one/modules/questions/controllers/QuestionController.java
@@ -1,5 +1,8 @@
 package com.porto.certific_one.modules.questions.controllers;
 
+import com.porto.certific_one.modules.questions.dtos.AlternativesResultDTO;
+import com.porto.certific_one.modules.questions.dtos.QuestionAlternativesDTO;
+import com.porto.certific_one.modules.questions.entities.Alternatives;
 import com.porto.certific_one.modules.questions.entities.Questions;
 import com.porto.certific_one.modules.questions.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/questions")
@@ -19,7 +23,30 @@ public class QuestionController {
 
 
     @GetMapping("/technology/{technology}")
-    public  List<Questions> getAllQuestionsByTechnology(@PathVariable String technology){
-        return questionRepository.findByTechnology(technology);
+    public  List<QuestionAlternativesDTO> getAllQuestionsByTechnology(@PathVariable String technology){
+        var result = questionRepository.findByTechnology(technology);
+
+        var mapResult = result.stream().map(question -> mapQuestioToDTO(question))
+                .collect(Collectors.toList());
+
+        return mapResult;
+    }
+
+    static QuestionAlternativesDTO mapQuestioToDTO(Questions questions) {
+        var questioResultDTO = new QuestionAlternativesDTO(
+                questions.getId(),
+                questions.getDescription(),
+                questions.getTechnology(),
+                questions.getAlternatives().stream().map(alternative -> mapAlternativeDTO(alternative))
+                        .collect(Collectors.toList())
+        );
+
+        return questioResultDTO;
+
+    }
+
+    static AlternativesResultDTO mapAlternativeDTO(Alternatives alternatives){
+        return new AlternativesResultDTO(alternatives.getId() , alternatives.getDescription());
+
     }
 }
